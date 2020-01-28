88f18c010874ff97db184529cf5eb0b427507d2e
Leonardo Antonio Miranda <leoamiranda2@gmail.com>
2020-01-28 19:23:08 -0300

    ajustes no código

--- routes/user.php
+++ routes/user.php
@@ -8,42 +8,47 @@ use Jwt\JwtProcess, Middleware\Jwt as JwtMiddleware;
 use Controllers\UserController;
 
 $app->get('/users', function (Request $request, Response $response, array $args) {
-  $atr = $request->getAttribute('payload');
   $userController = new UserController;
   
   if ( $userController->getAll() ) {
     return $response->withJson(array( "ok" => TRUE, "output" => $userController->output ));
   }
   return $response->withJson(array( "ok" => FALSE, "msg" => $userController->msg ));
-})->add(new JwtMiddleware());
+
+})->add(new JwtMiddleware);
 
 $app->get('/user/{id}', function (Request $request, Response $response, array $args) {
-  $atr = $request->getAttribute('payload');
+
   $userController = new UserController;
   
   if ( $userController->getById($args['id']) ) {
     return $response->withJson(array( "ok" => TRUE, "output" => $userController->output ));
   }
   return $response->withJson(array( "ok" => FALSE, "msg" => $userController->msg ));
-})->add(new JwtMiddleware());
+
+})->add(new JwtMiddleware);
 
 $app->post('/user', function (Request $request, Response $response) {
+
   $args = $request->getParams();
-  
   $userController = new UserController;
   
   if ( $userController->create($args) ) {
     return $response->withJson(array( "ok" => TRUE, "idInserido" => $userController->output ));
   }
   return $response->withJson(array( "ok" => FALSE, "msg" => $userController->msg ));
+
 });
 
 $app->post('/user/login', function (Request $request, Response $response) {
+
   $userController = new UserController;
+
   if ($userController->login($request->getParams())) {
       $jwt = new JwtProcess;
       $token = $jwt->encode($userController->output);
       return $response->withJson( array("ok" => TRUE, "output" => $userController->output, "token" => $token) );
   }
   return $response->withJson( array("ok" => FALSE, "output" => $userController->output) );
+
 });
