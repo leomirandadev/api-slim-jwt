50cbfb9a800e7e46674a42e97123f891343aa2c1
Leonardo Antonio Miranda <leoamiranda2@gmail.com>
2020-01-29 20:01:20 -0300

    inserir o ip automaticamente

--- setup.php
+++ setup.php
@@ -1,37 +1,39 @@
 <?php 
-  
-  $folderSettings = __DIR__.'/settings/';
-  $keysecret = uniqid(base64_encode(rand()));
 
-  $arqsSettings = [
-    array(
-      'arqName'=>'ConfigDb.php',
-      'conteudo'=>'<?php  
-      namespace Settings;
+$sock = socket_create(AF_INET, SOCK_DGRAM, SOL_UDP);
+socket_connect($sock, "8.8.8.8", 53);
+socket_getsockname($sock, $localAddr);
 
-      class ConfigDB{
-          protected function getDBName():string { return "api_php"; }
-          protected function getHost():string { return "192.168.0.10"; }
-          protected function getUser():string { return "root"; }    
-          protected function getPassword():string { return "root"; }
-      }
-      '
-    ),
-    array(
-      'arqName'=>'ConfigJwt.php',
-      'conteudo'=>'<?php  
-      namespace Settings;
-      class ConfigJwt{
-          protected function getSecretKey():string {
-            return md5("'.$keysecret.'");
-          }
-      }'
-    )
-  ];
+$folderSettings = __DIR__.'/settings/';
+$keysecret = uniqid(base64_encode(rand()));
 
-  include_once('SetupTools.php');
+$arqsSettings = [
+  array(
+    'arqName'=>'ConfigDb.php',
+    'conteudo'=>'<?php  
+    namespace Settings;
 
-  $setupTools = new SetupTools;
-  $setupTools->createFolderAndArqs($folderSettings, $arqsSettings);
+    class ConfigDB{
+        protected function getDBName():string { return "api_php"; }
+        protected function getHost():string { return "'.$localAddr.'"; }
+        protected function getUser():string { return "root"; }    
+        protected function getPassword():string { return "root"; }
+    }
+    '
+  ),
+  array(
+    'arqName'=>'ConfigJwt.php',
+    'conteudo'=>'<?php  
+    namespace Settings;
+    class ConfigJwt{
+        protected function getSecretKey():string {
+          return md5("'.$keysecret.'");
+        }
+    }'
+  )
+];
 
-?>
+include_once('SetupTools.php');
+
+$setupTools = new SetupTools;
+$setupTools->createFolderAndArqs($folderSettings, $arqsSettings);
